3f271ceccf07aa41b69d1b4f3a24cf2359484c5a
jeva33 <jonathaneva@gmail.com>
2018-01-03 15:55:12 -0800

    Big Move

---
+++ work/ec-case/rhino.php
@@ -0,0 +1,229 @@
+<?php include("/app/password_protect.php"); ?>
+
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href='https://fonts.googleapis.com/css?family=Playfair+Display:700,900|Fira+Sans:400,400italic' rel='stylesheet' type='text/css'>
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400,700" rel="stylesheet" type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Martel:200,300,400,600,700,800,900" rel="stylesheet">
+
+		<link rel="stylesheet" href="../../css/reset.css"> <!-- CSS reset -->
+		<link rel="stylesheet" href="../../css/style.css"> <!-- Resource style -->
+
+		<script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+		<script src="../../js/modernizr.js"></script>
+		<!-- <script src="js/vert_timeline.js"></script> -->
+		<script src="../../js/lightbox.js"></script>
+	</head>
+
+	<script type="text/javascript"> $(document).ready(function(){ $('.visible-content').fadeIn(1500);}); </script>
+
+		<body>
+
+		<div class="visible-content" style="display:none;">
+
+				<div class="case-title-caption">The MVP</div>
+
+				<div class="section">
+							<div class="caption-bottom">
+								The MVP sought to "create an app that can answer any company question, in any situation, at any place and at anytime."
+							</div>
+							<img src="img/1_mvp_A.jpg">
+				</div>
+
+				<div class="section-right">
+					<div class="mobile-preview-container-2">
+						<video width="286" height="492" autoplay="autoplay" loop="true">
+							<source src="img/mvpB_320.mp4" type="video/mp4" />
+						</video>
+						<div class="caption-right">
+								The legacy software was focused on educating employees about their benefit options. The new app would direct employees’ questions to customized answers and experts based on the employee's’ chosen benefits.
+						</div>
+					</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+							<div class="caption-bottom">
+								One of the biggest hurdles for the app to overcome was ensuring safe and secure storage of employees’ private data, including a year-long HIPPA approval process.
+							</div>
+							<img src="img/1_mvp_B.jpg">
+						</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+							<div class="caption-bottom">
+								The challenge was to make the sign-in process secure while allowing employees to easily and frequently log in. We chose to make biometrics an MVP requirement.
+							</div>
+							<img src="img/login-small.png" class="small-image">
+						</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+							<div class="caption-bottom">
+								I led internal stakeholder alignment across product, engineering and thought leadership teams. In order to lead this process, I created a physical collaboration space to facilitate cross-functional discussions around refinements to the app.
+							</div>
+							<img src="img/1_mvp_C.jpg">
+						</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+							<div class="caption-bottom">
+								While there was debate around the appropriate product naming strategy, the team decided to initially name the app after the company.
+							</div>
+							<!-- larger lightbox image and button -->
+							<a href="img/mvp-ui-large.png" class="lightbox_trigger"><span class="zoom-area"></span></a>
+								<!-- smaller project image -->
+							<img src="img/mvp-ui-small.png" class="small-image">
+						</div>
+				</div>
+
+				<div class="case-title-caption">“Move Search to the Center of Everything”</div>
+
+					<div class="section">
+							<div class="caption-bottom">
+								The next big release focused on featuring search as the centerpiece of the experience.
+							</div>
+							<img src="img/notebook-timeline-1.jpg">
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+							<img src="img/tabbar-gen3.png">
+						</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+							<div class="caption-bottom">
+								With the “search central” design, each user could be navigating different experiences in an ad-hoc manner. Therefore, the design had to simulate a clear “transportation” to and from different experience – i.e., speaking into the phone, charting different search results, etc.
+							</div>
+							<img src="img/search-ani4.jpg">
+						</div>
+				</div>
+
+				<div class="section-right-3">
+					<div class="mobile-preview-container">
+						<video style="margin-bottom: 54px;" width="286" height="508" controls controlsList="nodownload">
+							<source src="img/search_320.mp4" type="video/mp4" />
+						</video>
+						<div class="caption-right">
+								The previous story board lead to this initial animation. Working closely with the iOS and Android engineers, this animation illustrated the "unique" opening and closing of the search modal.
+						</div>
+					</div>
+				</div>
+
+				<div class="case-title-caption">Set the stage for the “Virtual Agent Bot”</div>
+
+				<div class="section">
+						<div class="project-image">
+							<div class="caption-bottom">
+								The next challenge was creating a smart "virtual agent" without an ‘intelligence layer' present. The app would need to train a user base to interact with a unique 'bot-driven' paradigm.
+							</div>
+							<img src="img/bot-notebook-combined2.jpg">
+						</div>
+				</div>
+
+				<div class="section-right-2">
+					<div class="mobile-preview-container">
+						<video width="286" height="508" autoplay="autoplay" loop="true">
+							<source src="img/chatbot.mp4" type="video/mp4" />
+						</video>
+						<div class="caption-right">
+								The "UX Learning Curve" is a term that I often use to describe the need for user experience paradigms and the position of UI elements to remain consistent over time. Understanding the plan for future feature allows those features to be easily rolled into the existing app without forcing the user base to continually re-learn how to use the app after each release. <br><br> In the case of the smart “virtual agent,” the initial version displayed a series of well-timed ellipses animations to mimic processing time, to indicate to the user that the system is processing their request. This was so that when the "intelligence layer" in future releases is layered on, then the experience will be seamless.
+						</div>
+					</div>
+				</div>
+
+				<div class="case-title-caption">Admin to Employee Publishing</div>
+
+				<div class="section">
+						<div class="project-image">
+							<img src="img/publications-notebook-admin2.jpg">
+						</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+						<div class="caption-bottom">
+							Next step was creating a push notification publishing system where HR departments could release timely and required information to employees without making it feel like 'required reading.' I led a work session with subject matter experts and thought leaders to map out different tasks around potential employee questions and possible HR responses.
+						</div>
+								<!-- larger lightbox image and button -->
+							<a href="img/publications-task-mapping-large.jpg" class="lightbox_trigger"><span class="zoom-area"></span></a>
+								<!-- smaller project image -->
+							<img src="img/publications-task-mapping-small.png" class="small-image">
+						</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+						<div class="caption-bottom">
+							Existing blogging and publishing software were researched to determine best practices and focused on a mobile first approach.
+						</div><!-- larger lightbox image and button -->
+							<a href="img/pub-admin-tool-large.png" class="lightbox_trigger"><span class="zoom-area"></span></a>
+								<!-- smaller project image -->
+							<img src="img/pub-admin-tool-small.png" class="small-image">
+						</div>
+				</div>
+
+				<div class="section">
+					<div class="video-container">
+						<div class="caption-bottom">
+							In refining the prototype, the team identified the need to build a publication index  and recipient tool.
+						</div>
+						<video width="620" height="452" autoplay="autoplay" loop="true">
+							<source src="img/pub5.mp4" type="video/mp4" />
+						</video>
+					</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+						<div class="caption-bottom">
+							The “News” Publishing Tool
+						</div>
+						<!-- larger lightbox image and button -->
+							<a href="img/pub-tool-large.png" class="lightbox_trigger"><span class="zoom-area"></span></a>
+								<!-- smaller project image -->
+							<img src="img/pub-tool-small.png" class="small-image">
+						</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+						<div class="caption-bottom">
+							The goal was to design both iterations of the publication, native mobile and web app, to look content rich but only limit the publisher to one image.
+						</div>
+						<img src="img/mobile-news-small.png">
+						</div>
+				</div>
+
+				<div class="case-title-caption">Timely and Urgent Push</div>
+
+				<div class="section">
+						<div class="project-image">
+						<div class="caption-bottom">
+							Employees receive various push notifications -- benefit updates, payroll notices, company holidays, etc. The app features a familiar user experience paradigm--timeline-- to organize the wide range of different content.
+						</div>
+						<img src="img/interaction-model-sketch-small2.png">
+						</div>
+				</div>
+
+				<div class="section">
+						<div class="project-image">
+						<div class="caption-bottom">
+							The app’s interaction model allows employees to assess content in real life with quick gestures (read or dismiss), thereby providing real-time metrics to HR teams on relevant topics.
+						</div>
+						<img src="img/interaction-guide.png">
+						</div>
+				</div>
+
+		</div>
+
+		</body>
+</html>
